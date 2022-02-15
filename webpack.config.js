e01b7380c63c81197717f4b8b0f817bd0d481ee6
milad <milad.xsar72@gmail.com>
2022-02-15 23:58:05 +0330

    create boilerplate for the project (project architecture)

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  mode: "production",
+  devtool: "source-map",
+  module: {
+    rules: [{ test: /\.css$/, use: ["style-loader", "css-loader"] }],
+  },
+};
