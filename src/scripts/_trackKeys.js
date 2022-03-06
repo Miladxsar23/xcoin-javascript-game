6beff8a381cdcbad60ccc248e3db67120eec3b7b
milad <milad.xsar72@gmail.com>
2022-03-06 23:15:11 +0330

    add trankKeys function for control keyboard event

---
+++ src/scripts/_trackKeys.js
@@ -0,0 +1,24 @@
+/**
+ * track keys event
+ */
+const arrowKeys = ["ArrowLeft", "ArrowRight", "ArrowUp"];
+
+/**
+ * @param {arrowKeys} keys
+ * @returns {down}
+ */
+
+function trankKeys(keys) {
+  const down = Object.create(null);
+  function track(event) {
+    if (keys.includes(event.key)) {
+      down[event.key] = event.type === "keydown";
+      event.preventDefault();
+    }
+  }
+  window.addEventListener("keydown", track);
+  window.addEventListener("keyup", track);
+  return down;
+}
+
+export default trankKeys;
